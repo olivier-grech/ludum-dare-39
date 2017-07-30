46a6dd9f1f7817ea95557c5df73fd7b2ea189d34
Olivier Grech <olivier.grech@gmail.com>
2017-07-30 16:28:56 +0200

    Added level change

--- Assets/LevelObjective.cs
+++ Assets/LevelObjective.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelObjective : MonoBehaviour {
+	
+	private GameManager m_GameManager;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		m_GameManager = GameManager.instance;
 	}
 	
 	// Update is called once per frame
@@ -16,8 +19,16 @@ public class LevelObjective : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-	
+
 		if (other.GetComponent<Ship>() != null)
+		{
 			Debug.Log("Niveau fini");
+			m_GameManager.m_CompletedLevels[m_GameManager.m_CurrentLevelIndex] = true;
+			for (int i = 0; i < 6; i++)
+				Debug.Log(m_GameManager.m_CompletedLevels[i]);
+			
+			m_GameManager.ChangeLevel(m_GameManager.m_CurrentLevelIndex + 1);
+		}
+			
 	}
 }

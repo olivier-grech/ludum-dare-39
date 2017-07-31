a139f1ea5ef01f55a09a053d6dc80f1137be20fd
Unknown <Carlos.filipe.Moreira.guedes@outlook.fr>
2017-07-31 17:28:41 +0200

    Added Force to asteroid

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+	public Rigidbody2D ThisRigidbody2D;
+	public float xForce;
+	public float yForce;
+	
+	// Use this for initialization
+	private void Awake()
+	{
+		ThisRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Start () {
+		ThisRigidbody2D.AddForce(new Vector2(xForce,yForce));
+		Debug.Log("I'm here");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
